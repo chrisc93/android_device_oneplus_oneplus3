nvertigo/android_device_oneplus_oneplus3t

releasetools: allow appending custom additions to updater-script.
Just add your edify functions in:

  releasetools/extra_script

Change-Id: I28fe0591a2e7becc9ee584c8dae55586726b7c57

BoardConfig.mk
@@ -225,5 +225,8 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+# custom additions to updater-script
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/oneplus/oneplus3t/releasetools/ota_from_target_files
+
 # inherit from the proprietary version
 -include vendor/oneplus/oneplus3t/BoardConfigVendor.mk

chrisc93/android_device_oneplus_oneplus3

BoardConfig.mk
@@ -230,5 +230,8 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
 
+# custom additions to updater-script
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/oneplus/oneplus3/releasetools/ota_from_target_files
+
 # inherit from the proprietary version
 -include vendor/oneplus/oneplus3/BoardConfigVendor.mk